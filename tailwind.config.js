a4e38619c1b8e602e343ea5ff574fa4caacb8263
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2023-01-18 14:11:18 -0300

    style: 💄 eslint adjusts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         roboto: ['Roboto', 'sans-serif'],
-        sofia: ['Sofia Sans', 'sans-serif']
-      }
+        sofia: ['Sofia Sans', 'sans-serif'],
+      },
     },
   },
   plugins: [],
